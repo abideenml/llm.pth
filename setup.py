bfc74029f77ec4b3915b8569510354a7df64fd7c
abideenml <zaiinn440@gmail.com>
2024-04-26 17:53:55 +0500

    setup file added

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="llm.pth",
+    version="0.1",
+    packages=find_packages(),
+    author="abideenml",
+    author_email="zaiinn440@gmail.com",
+    description="Implementation of various Autoregressive models, Research papers and techniques.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/abideenml/llm.pth",
+    install_requires=[
+        "transformers",
+        "datasets",
+        "torch",
+        "wandb",
+    ],
+    python_requires=">=3.8",
+)
