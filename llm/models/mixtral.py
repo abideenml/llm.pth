f19728fc0b5ba37cbeed8fed0670317ba98146e6
abideenml <zaiinn440@gmail.com>
2024-07-29 13:21:55 +0500

    mixtral architecture added

--- llm/models/mixtral.py
+++ llm/models/mixtral.py
@@ -1,12 +1,7 @@
 from __future__ import annotations
-import math
+import math, torch, lightning as L
 from dataclasses import dataclass
-import torch
-import torch.nn as nn
-from torch import Tensor
 from typing import Any
-import torch.nn.functional as F
-import lightning as L
 from lightning.pytorch.utilities.model_summary import ModelSummary
 
 @dataclass
@@ -25,7 +20,8 @@ class MOEConfig:
     num_experts: int = 4
     num_experts_per_tok: int = 2
 
-class SwiGLU(nn.Module):
+
+class SwiGLU(torch.nn.Module):
     def __init__(
         self,
         dim: int,
@@ -41,29 +37,28 @@ class SwiGLU(nn.Module):
         order in which W1,W2,W3 are multiplied is as per llama (for compatiblity)
         """
         super().__init__()
-
         if hidden_dim is None:
             hidden_dim = 4 * dim
             hidden_dim = int(2 * hidden_dim / 3)
             hidden_dim = multiple_of * ((hidden_dim + multiple_of - 1) // multiple_of)
 
-        self.w1 = nn.Linear(dim, hidden_dim, bias=bias)
-        self.w2 = nn.Linear(hidden_dim, dim, bias=bias)
-        self.w3 = nn.Linear(dim, hidden_dim, bias=bias)
-        self.dropout = nn.Dropout(dropout) if dropout else lambda x: x
+        self.w1 = torch.nn.Linear(dim, hidden_dim, bias=bias)
+        self.w2 = torch.nn.Linear(hidden_dim, dim, bias=bias)
+        self.w3 = torch.nn.Linear(dim, hidden_dim, bias=bias)
+        self.dropout = torch.nn.Dropout(dropout) if dropout else lambda x: x
 
     def forward(self, x):
-        return self.dropout(self.w2(F.silu(self.w1(x)) * self.w3(x)))
+        return self.dropout(self.w2(torch.nn.functional.F.silu(self.w1(x)) * self.w3(x)))
 
 
-class RMSNorm(nn.Module):
+class RMSNorm(torch.nn.Module):
     def __init__(self, dim: int, eps: float = 1e-5):
         super().__init__()
         """
         Paper: https://arxiv.org/abs/1910.07467
         """
         self.eps = eps
-        self.weight = nn.Parameter(torch.ones(dim))
+        self.weight = torch.nn.Parameter(torch.ones(dim))
 
     def _norm(self, x: torch.Tensor):
         return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
@@ -76,10 +71,8 @@ def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
     freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
     # [: (dim // 2)] for odd number truncation
     # torch.arange(0, dim, 2) -> 2(i-1)//d while i= 1,2,..,(d//2)
-
     t = torch.arange(end, device=freqs.device)
     freqs = torch.outer(t, freqs).float()  # gives diffrent angle vector
-
     # e^it = cos(t) + i sin(t)
     freqs_cos = torch.cos(freqs)  # real
     freqs_sin = torch.sin(freqs)  # imaginary
@@ -101,55 +94,28 @@ def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
 
 
 def apply_rope(k, q, cis):
-    # Idea suppose vector v = [x,y,x1,y1,...] # v.shape = dim
-    # convert vetor into complex num # ie two vec one real, one imagery
-    # [x,y,x1,y1,...] -> x+iy, x1+iy1
-    # Multiplying by complex num == roatate vector
-    # => (x + iy) * (cos + isin) -> x'+iy'
-    # restack
-    # x'+iy' -> [x',y',x1',y1'...]
-    # you roated vector in chunks of two lfg!!!
     _, seq_len, _, _ = q.shape
-
     freqs_cos, freqs_sin = cis
     freqs_cos, freqs_sin = freqs_cos[:seq_len], freqs_sin[:seq_len]
-
-    #  rehsape a shape (...,n )-> (..., n//2,2)
-    q_cis = q.float().reshape(
-        q.shape[:-1] + (-1, 2)
-    )  # (B,T,nhead,C) -> (B,T,nhead,Cc,2) # Cc = C//2
+    q_cis = q.float().reshape( q.shape[:-1] + (-1, 2))  # (B,T,nhead,C) -> (B,T,nhead,Cc,2) # Cc = C//2
     k_cis = k.float().reshape(k.shape[:-1] + (-1, 2))  # (B,T,nhead,C) -> (B,T,nhead,Cc,2)
-
     xq_r, xq_i = q_cis.unbind(-1)  # (B,T,nhead,Cc,2) -> ((B,T,Cc), (B,T,Cc)) split into two tuple
     xk_r, xk_i = k_cis.unbind(-1)  # (B,T,nhead,Cc,2) -> ((B,T,Cc), (B,T,Cc))
-
     freqs_cos = reshape_for_broadcast(freqs_cos, xq_r)  # freqs.shape = (1,T,1,Cc)
     freqs_sin = reshape_for_broadcast(freqs_sin, xq_r)
-
-    # e+if = (a+ib) * (c+di) = (ac-bd) + i (ad+bc)
-    # a = xq_r , b = xq_i
-    # c = fcos , d = fsin
-    # ...
-    # e = (ac-bd) = xq_r * freqs_cos - xq_i * freqs_sin
-    # f = (c+di)  = xq_r * freqs_sin + xq_i * freqs_cos
-
     xq_out_r = xq_r * freqs_cos - xq_i * freqs_sin  # (ac-bd)   # shape =  # (B,T,nhead,Cc)
     xq_out_i = xq_r * freqs_sin + xq_i * freqs_cos  # (ad+bc) * i
     xk_out_r = xk_r * freqs_cos - xk_i * freqs_sin  # (ac-bd)
     xk_out_i = xk_r * freqs_sin + xk_i * freqs_cos  # (ad+bc) * i
-
-    # now we stack r,i -> [r,i,r2,i2]
     xq_out = torch.stack([xq_out_r, xq_out_i], dim=-1)  # (B,T,nhead,Cc,2)
     xk_out = torch.stack([xk_out_r, xk_out_i], dim=-1)  # (B,T,nhead,Cc,2)
-
-    # flatten last two dimensions
     xq_out = xq_out.flatten(3)  # (B,T,nhead,C)
     xk_out = xk_out.flatten(3)  # (B,T,nhead,C)
 
     return xq_out.type_as(q), xk_out.type_as(q)
 
 
-class Attention(nn.Module):
+class Attention(torch.nn.Module):
     def __init__(self, model_args: MOEConfig):
         super().__init__()
         d_model = model_args.d_model
@@ -160,34 +126,26 @@ class Attention(nn.Module):
         )
         assert self.num_heads % self.num_kv_heads == 0
         self.num_queries_per_kv = self.num_heads // self.num_kv_heads
-
-        self.key = nn.Linear(d_model, self.head_dim * self.num_heads)
-        self.query = nn.Linear(d_model, self.head_dim * self.num_kv_heads)
-        self.value = nn.Linear(d_model, self.head_dim * self.num_kv_heads)
-        self.proj = nn.Linear(d_model, d_model, model_args.bias)
-
-        self.attn_dropout = nn.Dropout(model_args.dropout)
-        self.res_dropout = nn.Dropout(model_args.dropout)
-
+        self.key = torch.nn.Linear(d_model, self.head_dim * self.num_heads)
+        self.query = torch.nn.Linear(d_model, self.head_dim * self.num_kv_heads)
+        self.value = torch.nn.Linear(d_model, self.head_dim * self.num_kv_heads)
+        self.proj = torch.nn.Linear(d_model, d_model, model_args.bias)
+        self.attn_dropout = torch.nn.Dropout(model_args.dropout)
+        self.res_dropout = torch.nn.Dropout(model_args.dropout)
         self.flash_attn = hasattr(torch.nn.functional, "scaled_dot_product_attention")
 
     def forward(self, x: torch.Tensor, mask: torch.Tensor, freqs_cis) -> torch.Tensor:
         batch, seq_len, d_model = x.shape
-
-        k: torch.Tensor  # type hint for lsp
-        q: torch.Tensor  # ignore
+        k: torch.Tensor  
+        q: torch.Tensor  
         v: torch.Tensor
-
         k = self.key(x)
         q = self.query(x)
         v = self.value(x)
 
-        k = k.view(
-            batch, seq_len, self.num_heads, self.head_dim
-        )  # shape = (B, seq_len, num_heads, head_dim)
+        k = k.view(batch, seq_len, self.num_heads, self.head_dim)  # shape = (B, seq_len, num_heads, head_dim)
         q = q.view(batch, seq_len, self.num_heads, self.head_dim)
         v = v.view(batch, seq_len, self.num_heads, self.head_dim)
-
         q, k = apply_rope(q, k, freqs_cis)
 
         # Grouped Query Attention
@@ -200,31 +158,21 @@ class Attention(nn.Module):
         v = v.transpose(1, 2)
 
         if self.flash_attn:
-            output = torch.nn.functional.scaled_dot_product_attention(
-                q,
-                k,
-                v,  # order impotent
-                attn_mask=None,
-                dropout_p=self.attn_dropout.p if self.training else 0.0,
-                is_causal=True,
-            )
+            output = torch.nn.functional.scaled_dot_product_attention(q,k,v,attn_mask=None,dropout_p=self.attn_dropout.p if self.training else 0.0,is_causal=True)
         else:
             attn_mtx = torch.matmul(q, k.transpose(2, 3)) / math.sqrt(self.head_dim)
             attn_mtx = attn_mtx + mask[:, :, :seq_len, :seq_len]
-            attn_mtx = F.softmax(attn_mtx.float(), dim=-1).type_as(k)
+            attn_mtx = torch.nn.functionalF.softmax(attn_mtx.float(), dim=-1).type_as(k)
             attn_mtx = self.attn_dropout(attn_mtx)
-
             output = torch.matmul(attn_mtx, v)  # (batch, n_head, seq_len, head_dim)
 
         # restore time as batch dimension and concat heads
         output = output.transpose(1, 2).contiguous().view(batch, seq_len, d_model)
-
-        # final projection into the residual stream
         output = self.proj(output)
         output = self.res_dropout(output)
         return output
 
-class MoE(nn.Module):
+class MoE(torch.nn.Module):
     def __init__(
         self,
         dim: int,
@@ -236,57 +184,41 @@ class MoE(nn.Module):
         super().__init__()
         self.num_experts = num_experts
         self.num_experts_per_tok = num_experts_per_tok
-
         mlp_block = SwiGLU
-
-        self.experts = nn.ModuleList([mlp_block(dim, hidden_dim) for i in range(num_experts)])
-        self.gate = nn.Linear(dim, num_experts, bias=False)
+        self.experts = torch.nn.ModuleList([mlp_block(dim, hidden_dim) for i in range(num_experts)])
+        self.gate = torch.nn.Linear(dim, num_experts, bias=False)
 
     def forward(self, x: torch.Tensor):
         batch_size, seq_len, dim = x.shape  # (batch_size, seq_len, dim)
-
         # (batch_size , seq_len, dim) -> (batch_size * seq_len, dim)
         x = x.view(batch_size * seq_len, dim)
-
         # (batch_size * seq_len, dim) -> (batch_size * seq_len, num_experts)
         scores = self.gate(x)
-
         # expert_weights -> (batch_size * seq_len, num_experts_per_tok)
         # expert_indices -> (batch_size * seq_len, num_experts_per_tok)
         expert_weights, expert_indices = torch.topk(scores, self.num_experts_per_tok, dim=-1)
-
         # -> (batch_size * seq_len, num_experts_per_tok)
         expert_weights = expert_weights.softmax(dim=-1)
-
         #  -> (batch_size * seq_len * num_experts_per_tok ) 1D
         flat_expert_indices = expert_indices.view(-1)
-
         # (batch_size * seq_len, dim) -> (batch_size * seq_len * num_experts_per_tok, dim)
         # create copied of inputs for each expert
         x = x.repeat_interleave(self.num_experts_per_tok, dim=0)
-
         # (total_tokens,dim)
         output = torch.empty_like(x, dtype=x.dtype, device=x.device)
-
         for idx, expert in enumerate(self.experts):
             # filtered_x - selected toks that to be sent to nth expert
             filtered_x = x[flat_expert_indices == idx]
             output[flat_expert_indices == idx] = expert(filtered_x)
-
-        # ->B,T,num_experts_per_tok,dim
         output = output.view(*expert_weights.shape, -1)
         expert_weights = expert_weights.unsqueeze(-1)
-
         output = output * expert_weights
-
-        # sum up experts outputs
-        # batch_size * seq_len, num_experts_per_tok, dim -> batch_size * seq_len, dim
         output = output.sum(dim=1)
 
         return output
     
 
-class Block(nn.Module):
+class Block(torch.nn.Module):
     def __init__(self, model_args: MOEConfig):
         super().__init__()
 
@@ -300,7 +232,6 @@ class Block(nn.Module):
             dropout=model_args.dropout,
             bias=model_args.bias,
             )
-
         self.norm1 = RMSNorm(model_args.d_model)
         self.norm2 = RMSNorm(model_args.d_model)
 
@@ -310,25 +241,16 @@ class Block(nn.Module):
         return x
 
 
-class Mixtral(nn.Module):
+class Mixtral(torch.nn.Module):
     def __init__(self, model_args: MOEConfig, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-
         self.config = model_args
-
-        self.token_emb = nn.Embedding(model_args.vocab_size, model_args.d_model)
-
-        self.layers = nn.ModuleList([Block(model_args) for _ in range(model_args.num_layers)])
-
+        self.token_emb = torch.nn.Embedding(model_args.vocab_size, model_args.d_model)
+        self.layers = torch.nn.ModuleList([Block(model_args) for _ in range(model_args.num_layers)])
         self.norm = RMSNorm(model_args.d_model)
-        self.vocab_proj = nn.Linear(model_args.d_model, model_args.vocab_size, bias=False)
-
+        self.vocab_proj = torch.nn.Linear(model_args.d_model, model_args.vocab_size, bias=False)
         self.token_emb.weight = self.vocab_proj.weight
-
-        self.cis = precompute_freqs_cis(
-            model_args.d_model // model_args.num_heads, model_args.seq_len * 2
-        )
-
+        self.cis = precompute_freqs_cis(model_args.d_model // model_args.num_heads, model_args.seq_len * 2)
         if hasattr(torch.nn.functional, "scaled_dot_product_attention"):
             print("WARNING: using slow attention | upgrade pytorch to 2.0 or above")
             mask = torch.full((1, 1, model_args.seq_len, model_args.seq_len), float("-inf"))
@@ -339,7 +261,7 @@ class Mixtral(nn.Module):
 
         self.apply(self._init_weights)
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor, targets=None):
         batch, seqlen = x.shape
         x = self.token_emb(x)
         device = self.token_emb.weight.device
@@ -349,15 +271,18 @@ class Mixtral(nn.Module):
             x = layer(x, self.mask, freqs_cis)
 
         x = self.norm(x)
-        x = self.vocab_proj(x)
-        return x
+        logits = self.vocab_proj(x)
+        loss = None
+        if targets is not None:
+            loss = torch.nn.functional.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss
 
     def _init_weights(self, module):
-        if isinstance(module, nn.Linear):
+        if isinstance(module, torch.nn.Linear):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
             if module.bias is not None:
                 torch.nn.init.zeros_(module.bias)
-        elif isinstance(module, nn.Embedding):
+        elif isinstance(module, torch.nn.Embedding):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
 class Model(L.LightningModule):
@@ -377,7 +302,7 @@ def convert_int_to_shortened_string(num):
     else:
         return f"{num / 1000000000000:.1f}T"
 
-def model_summary(model: nn.Module, print_summary=False):
+def model_summary(model: torch.nn.Module, print_summary=False):
     "conver normal model to lightning model and print model summary"
     model = Model(model)
     summary = ModelSummary(model)
@@ -394,8 +319,5 @@ if __name__ == "__main__":
     device = "mps"
     model = Mixtral(MOEConfig).to(device)
     model = torch.compile(model)
-
-    print("-" * 100)
     print(model)
     print(model_summary(model))
-    print("-" * 100)
